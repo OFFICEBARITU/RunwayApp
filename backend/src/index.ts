7e98eb3623400c618846b54b993f32f3cfe83a1f
OFFICEBARITU <officebaritu@protonmail.com>
2026-05-02 02:36:12 -0300

    Fix trust proxy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,7 @@ import { paymentStatusRouter } from './routes/paymentStatus'
 dotenv.config()
 
 const app = express()
+app.set('trust proxy', 1)
 const PORT = process.env.PORT || 4000
 
 // Security
